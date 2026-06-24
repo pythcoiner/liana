bd1737d1ea7220dbd4d7748ab5fb3c5a6d994d83
pythcoiner <pythcoiner@proton.me>
2026-06-24 06:49:04 -0400

    business-installer: center and fix the scrollable-list content band to the entry width

--- liana-business/business-installer/src/views/template_builder/mod.rs
+++ liana-business/business-installer/src/views/template_builder/mod.rs
@@ -168,7 +168,6 @@ pub fn template_builder_view(state: &State) -> Element<'_, Msg> {
         list_content,
         pinned_content,
         footer_content,
-        true,
         Some(Msg::NavigateBack),
     )
 }
